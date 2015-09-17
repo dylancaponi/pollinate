1483f4ad23c3d8f4abb92c26afeec5871e401492
Dylan Caponi <dylan.caponi+github@gmail.com>
2015-09-17 01:51:47 +0000

    changed upload threshold and no shutdown.sh in init.sh

--- inits.sh
+++ inits.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo 'starting initMain.sh'
 bash /media/usb/pollinate/initMain.sh &
-echo 'starting initShutdown.sh'
-bash /media/usb/pollinate/initShutdown.sh &
+#echo 'starting initShutdown.sh'
+#bash /media/usb/pollinate/initShutdown.sh &
---
+++ timer.py
@@ -0,0 +1,9 @@
+import time
+
+startTime = time.time()
+time.sleep(10)
+endTime = time.time()
+
+print startTime
+print endTime
+print endTime-startTime
