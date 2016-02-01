7a031ebeb47d512fbbd3c56b696965287ba09b72
Dylan Caponi <dc@MacBook-Pro-2.local>
2016-02-01 01:04:16 -0600

    update main

--- main.py
+++ main.py
@@ -1,120 +1,129 @@
 import serial, os, boto, uuid
 import time
+import thread
+from datetime import datetime
+from datetime import timedelta
 from time import gmtime, strftime, sleep
 from boto.s3.key import Key 
 
+# Store AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY in ~/.aws/credentials
 
-
-connect_retry_time = 5
-oldfile = ""
-
-
-
-# store AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY in ~/.aws/credentials
-print "connecting to aws"
-startTime = time.time()
-s3 = boto.connect_s3()
-print startTime
-#sleep(connect_retry_time)
-
-
-# check if bucket exists, if not then create
-bucket_name = "wenis" # per hive you only get 100
-if s3.lookup(bucket_name):
-	print bucket_name + " already exists"
-	bucket = s3.lookup(bucket_name)
-else:
-	endTime = time.time()
-	if endTime - startTime > 30:
-		print 'it was bigger'
-		os._exit()
-	# fix - this could fail if someone else owns bucket
-	print "creating new bucket with name: " + bucket_name
-	bucket = s3.create_bucket(bucket_name)
-
-k = Key(bucket)
-
-
-# checks for standard osx and raspi serial port names
+# Check for standard osx and raspi serial port names
 def get_serial_port():
 	ser_devs = [dev for dev in os.listdir('/dev') if dev.startswith('ttyACM') or dev.startswith('tty.usbmodem')]
 	if len(ser_devs) > 0:
 		return '/dev/' + ser_devs[0]
 	return None
 
-
-
-# collect data via serial
-def collect_upload(ser):
-	global oldfile
-	global k
-	cleanup = 0
-	num_writes = 0
-	num_write_upload_threshold = 50
-	while(1):
-
-		# update filename, won't change if same day as last write
-		# fix - write to usb directory
-		newfile = strftime("%Y-%m-%d") + ".csv" # add a minute to test
-		if newfile != oldfile:
-			print 'creating new file: ' + newfile
-			deletefile = oldfile
-			cleanup = 1
-			oldfile = newfile
-
-
-		data = ser.readline()
-		#print data
-		data = data.replace("\t",",")
-		data = strftime("%H-%M-%S") + ',' + data
-		print data
-
-		# write to file with day, files will auto create daily w log
-		with open(newfile, 'a') as f:
-			f.write(data)
-
-		# write to S3.  there are no object limits or really upload rate limits.
-		# sucks to have a ton of objects though, maybe upload data every minute?
-		if num_writes == num_write_upload_threshold:
-			# write to aws
-			print 'uploading to aws'
-			k.key = '/data/count/' + newfile
-
-			# get all the data from file
-			with open(newfile, 'r') as f:
-				all_data_from_file = f.read()
-
-			# fix - try to set contents, except - reconnect to aws
-			k.set_contents_from_string(all_data_from_file)
-			all_data_from_file = ""
-
-			num_writes = 0
-
-			if cleanup == 1 and deletefile != "":
-				# try to delete old file to save space only after uploading to S3
-				# fix - add a check that upload was successful
-				print 'deleting old file:' + deletefile
-				os.remove(deletefile)
-				cleanup = 0
-				
-
-		num_writes += 1
-
-while(1):
-	# try to connect to usb
+# Connect to teensy
+def connect_to_serial_port():
 	ser = serial.Serial(get_serial_port(),9600)
-	if ser.name != None:
+	if ser.name:
 		print 'connected to ' + ser.name
-		try:
-			collect_upload(ser)
-		except Exception, e:
-			print e
-			pass
-		
+		return ser
 	else:
-		print 'failed to connect.  waiting ' + str(connect_retry_time) + ' seconds.'
-		# upload zeros/msg/log to aws?
-		sleep(connect_retry_time)
+		print 'not connected to teensy'
+
+# Read line from serial port and format
+def read_format_data(ser):
+	data = ser.readline()
+	data = data.replace("\t",",")
+	data = strftime("%Y:%m:%d %H:%M:%S") + ',' + data
+	return data
+
+# Write data to day file
+def write_to_file(data):
+	filename = strftime("%Y-%m-%d") + ".csv"
+	with open(filename, 'a') as f:
+		f.write(data)
+
+# Connect to AWS
+def connect_to_aws():
+	print "connecting to aws"
+	s3 = boto.connect_s3()
+	return s3
+
+# Check if bucket exists, if not then create
+def check_bucket(s3, bucket_name):
+	print 'looking up bucket:', bucket_name# per hive you only get 100
+	if s3.lookup(bucket_name):
+		print bucket_name + " already exists"
+		bucket = s3.lookup(bucket_name)
+	return bucket
+
+# Needs to be done in thread
+def write_file_to_s3(filename):
+	print 'uploading to S3'
+	filename = filename + ".csv"
+	s3 = connect_to_aws()
+	bucket = check_bucket(s3, bucket_name)
+	k = Key(bucket)
+	k.key = '/data/count/' + filename
+
+	# get all the data from file
+	with open(filename, 'r') as f:
+		all_data_from_file = f.read()
+	print 'write', filename, 'to s3'
+	k.set_contents_from_string(all_data_from_file)
+
+def get_minute():
+	return strftime('%M')
+
+def get_date():
+	return strftime("%Y-%m-%d")
+
+# Initialization vars
+bucket_name = 'wenis'
+connect_retry_time = 5
+last_minute = get_minute()
+last_date = get_date() 
+# FOR TESTING - REMOVE
+last_date = "2016-01-30"
+filename = None
+ser = None
+
+
+while True:
+
+	# Read data from Teensy
+	if ser:
+		try:
+			data = read_format_data(ser)
+			write_to_file(data)
+			print data
+		except Exception, e:
+			print "Failed to read in the usual spot: ", e
+	else:
+		ser = connect_to_serial_port()
+		time.sleep(1)
+
+	# Execute daily tasks
+	date_now = get_date()
+	if date_now != last_date:
+		print 'day:',date_now
+		thread.start_new_thread(write_file_to_s3,(last_date,))
+		last_date = date_now
+
+	# Execute minutely tasks
+	minute_now = get_minute()
+	if minute_now != last_minute:
+		print 'minute:',minute_now
+		print 'write to mysql in thread'
+		last_minute = minute_now
+
+	
+
+	# time.sleep(10)
+
+
+
+
+
+
+
+
+		
+	
 
 	
 
---
+++ old_main.py
@@ -0,0 +1,125 @@
+import serial, os, boto, uuid
+import time
+from time import gmtime, strftime, sleep
+from boto.s3.key import Key 
+
+
+
+connect_retry_time = 5
+oldfile = ""
+
+
+
+# store AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY in ~/.aws/credentials
+print "connecting to aws"
+startTime = time.time()
+s3 = boto.connect_s3()
+print startTime
+#sleep(connect_retry_time)
+
+
+# check if bucket exists, if not then create
+bucket_name = "wenis" # per hive you only get 100
+if s3.lookup(bucket_name):
+	print bucket_name + " already exists"
+	bucket = s3.lookup(bucket_name)
+else:
+	endTime = time.time()
+	if endTime - startTime > 30:
+		print 'it was bigger'
+		os._exit()
+	# fix - this could fail if someone else owns bucket
+	print "creating new bucket with name: " + bucket_name
+	bucket = s3.create_bucket(bucket_name)
+
+k = Key(bucket)
+
+
+# checks for standard osx and raspi serial port names
+def get_serial_port():
+	ser_devs = [dev for dev in os.listdir('/dev') if dev.startswith('ttyACM') or dev.startswith('tty.usbmodem')]
+	if len(ser_devs) > 0:
+		return '/dev/' + ser_devs[0]
+	return None
+
+
+
+# collect data via serial
+def collect_upload(ser):
+	global oldfile
+	global k
+	cleanup = 0
+	num_writes = 0
+	num_write_upload_threshold = 50
+	while(1):
+
+		# update filename, won't change if same day as last write
+		# fix - write to usb directory
+		newfile = strftime("%Y-%m-%d") + ".csv" # add a minute to test
+		if newfile != oldfile:
+			print 'creating new file: ' + newfile
+			deletefile = oldfile
+			cleanup = 1
+			oldfile = newfile
+
+
+		data = ser.readline()
+		#print data
+		data = data.replace("\t",",")
+		data = strftime("%H-%M-%S") + ',' + data
+		print data
+
+		# write to file with day, files will auto create daily w log
+		with open(newfile, 'a') as f:
+			f.write(data)
+
+		# write to S3.  there are no object limits or really upload rate limits.
+		# sucks to have a ton of objects though, maybe upload data every minute?
+		if num_writes == num_write_upload_threshold:
+			# write to aws
+			print 'uploading to aws'
+			k.key = '/data/count/' + newfile
+
+			# get all the data from file
+			with open(newfile, 'r') as f:
+				all_data_from_file = f.read()
+
+			# fix - try to set contents, except - reconnect to aws
+			k.set_contents_from_string(all_data_from_file)
+			all_data_from_file = ""
+
+			num_writes = 0
+
+			if cleanup == 1 and deletefile != "":
+				# try to delete old file to save space only after uploading to S3
+				# fix - add a check that upload was successful
+				print 'deleting old file:' + deletefile
+				os.remove(deletefile)
+				cleanup = 0
+				
+
+		num_writes += 1
+
+while(1):
+	# try to connect to usb
+	ser = serial.Serial(get_serial_port(),9600)
+	if ser.name != None:
+		print 'connected to ' + ser.name
+		try:
+			collect_upload(ser)
+		except Exception, e:
+			print e
+			pass
+		
+	else:
+		print 'failed to connect.  waiting ' + str(connect_retry_time) + ' seconds.'
+		# upload zeros/msg/log to aws?
+		sleep(connect_retry_time)
+
+	
+
+
+
+
+
+
